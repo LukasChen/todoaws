da89def115e844509cdccf7f6d88834f3f24b950
Lukas Chen <villagelukaschen@gmail.com>
2023-01-22 16:25:24 +0800

    edit data

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,15 @@ import type { Ref } from 'vue';
 import { DataStore } from '@aws-amplify/datastore';
 import { Authors, LazyAuthors } from './models';
 
-let authors : Ref<LazyAuthors[]> = ref([]);
+let authors: Ref<LazyAuthors[]> = ref([]);
+let firstName: Ref<string> = ref("Sigma");
+let lastName: Ref<string> = ref("Male");
 
 async function createAuthor() {
   await DataStore.save(
     new Authors({
-      "first_name": "Sigma",
-      "last_name": "Gay",
+      "first_name": firstName.value,
+      "last_name": lastName.value,
       "Posts": [],
     })
   );
@@ -40,15 +42,17 @@ onMounted(() => {
 
 <template>
   <div>
-  <h3>Hello World</h3>
-  <button class="btn" @click="createAuthor">Create Author</button>
-  <button class="btn btn-success" @click="queryAuthors">Fetch Authors</button>
-  <ul>
-    <li v-for="author in authors" :key="author.id">
-      {{ author.first_name }} {{ author.last_name }} 
-      <button class="btn btn-danger" @click="deleteAuthor(author.id)">Delete</button>
-    </li>
-  </ul>
+    <h3>Hello World</h3>
+    <button class="btn" @click="createAuthor">Create Author</button>
+    <button class="btn btn-success" @click="queryAuthors">Fetch Authors</button>
+    <ul>
+      <li v-for="author in authors" :key="author.id">
+        {{ author.first_name }} {{ author.last_name }}
+        <button class="btn btn-danger" @click="deleteAuthor(author.id)">Delete</button>
+      </li>
+    </ul>
+    <input class="form-control" v-model="firstName" />
+    <input class="form-control" v-model="lastName" />
   </div>
 </template>
 
@@ -58,9 +62,11 @@ onMounted(() => {
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
